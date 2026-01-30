e85a63ccb8747a40c5f511230d619bd67c63e876
Wassim-dxdydz <abahriwassim44@gmail.com>
2026-01-30 16:52:55 +0100

    Fix release

--- Scripts/income_predictor.py
+++ Scripts/income_predictor.py
@@ -5,7 +5,9 @@ It initializes and launches the GUI for processing socio-economic data,
 training the Random Forest model, generating visualizations, and
 reviewing model performance.
 """
+import multiprocessing
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     from gui import launch_gui
     launch_gui()
